3adcbc1aa2b0700258f6e96b3afe482851af3f1d
io-dario <dario@fabd.tech>
2025-05-29 12:03:23 +0200

    removed unused files

--- script.js
+++
@@ -1,7 +0,0 @@
-// script.js
-document.addEventListener('DOMContentLoaded', () => {
-  const intro = document.querySelector('.intro');
-  setTimeout(() => {
-    intro.classList.add('visible');
-  }, 500);
-});
